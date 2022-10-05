5fa3dc66fee03343f2e9c7525ff0c9158c9feed2
Brian Akins <205350+bakins@users.noreply.github.com>
2022-10-05 09:39:59 -0400

    refactor test to use proto package

--- go.mod
+++ go.mod
@@ -9,8 +9,9 @@ require (
 	github.com/stretchr/testify v1.8.0
 	go.opentelemetry.io/collector v0.61.0
 	go.opentelemetry.io/collector/pdata v0.61.0
-	go.opentelemetry.io/collector/semconv v0.61.0
+	go.opentelemetry.io/proto/otlp v0.19.0
 	go.uber.org/zap v1.23.0
+	google.golang.org/protobuf v1.28.1
 )
 
 require (
@@ -37,6 +38,5 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220930163606-c98284e70a91 // indirect
 	google.golang.org/grpc v1.49.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- otlptcp_test.go
+++ otlptcp_test.go
@@ -14,7 +14,8 @@ import (
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/pdata/ptrace"
-	semconv "go.opentelemetry.io/collector/semconv/v1.5.0"
+	tracepb "go.opentelemetry.io/proto/otlp/trace/v1"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/bakins/otlptcpreceiver/internal/sharedcomponent"
 )
@@ -49,9 +50,23 @@ func TestTraces(t *testing.T) {
 		assert.NoError(t, tr.Shutdown(ctx))
 	}()
 
-	marshaller := ptrace.NewProtoMarshaler()
+	traces := tracepb.TracesData{
+		ResourceSpans: []*tracepb.ResourceSpans{
+			{
+				ScopeSpans: []*tracepb.ScopeSpans{
+					{
+						Spans: []*tracepb.Span{
+							{
+								Name: "testing1234",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 
-	data, err := marshaller.MarshalTraces(traceOtlp)
+	data, err := proto.Marshal(&traces)
 	require.NoError(t, err)
 
 	otlp := tr.(*sharedcomponent.SharedComponent).Unwrap().(*otlpReceiver)
@@ -75,6 +90,8 @@ func TestTraces(t *testing.T) {
 	}
 
 	require.Equal(t, 1, sink.SpanCount())
+	name := sink.AllTraces()[0].ResourceSpans().At(0).ScopeSpans().At(0).Spans().At(0).Name()
+	require.Equal(t, "testing1234", name)
 }
 
 type traceWrapper struct {
@@ -91,20 +108,3 @@ func (w *traceWrapper) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 
 	return w.Traces.ConsumeTraces(ctx, td)
 }
-
-var traceOtlp = func() ptrace.Traces {
-	td := ptrace.NewTraces()
-	rs := td.ResourceSpans().AppendEmpty()
-	rs.Resource().Attributes().PutString(semconv.AttributeHostName, "testHost")
-	spans := rs.ScopeSpans().AppendEmpty().Spans()
-	span1 := spans.AppendEmpty()
-	span1.SetTraceID([16]byte{0x5B, 0x8E, 0xFF, 0xF7, 0x98, 0x3, 0x81, 0x3, 0xD2, 0x69, 0xB6, 0x33, 0x81, 0x3F, 0xC6, 0xC})
-	span1.SetSpanID([8]byte{0xEE, 0xE1, 0x9B, 0x7E, 0xC3, 0xC1, 0xB1, 0x74})
-	span1.SetParentSpanID([8]byte{0xEE, 0xE1, 0x9B, 0x7E, 0xC3, 0xC1, 0xB1, 0x73})
-	span1.SetName("testSpan")
-	span1.SetStartTimestamp(1544712660300000000)
-	span1.SetEndTimestamp(1544712660600000000)
-	span1.SetKind(ptrace.SpanKindServer)
-	span1.Attributes().PutInt("attr1", 55)
-	return td
-}()
